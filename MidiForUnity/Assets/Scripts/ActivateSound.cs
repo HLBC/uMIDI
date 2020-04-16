3e95076446fe628bd0c1992807495a35833d5a05
Sulamke <Sulamkegan@gmail.com>
2020-04-15 20:18:43 -0400

    Cubes play sounds

---
+++ MidiForUnity/Assets/Scripts/ActivateSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateSound : MonoBehaviour
+{
+    [SerializeField] private GameObject sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && !other.isTrigger)
+            {
+            sound.SetActive(false);
+            sound.SetActive(true);
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
